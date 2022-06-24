9947fc5524905b965b436305cc75853c761e9bce
guilhermefrag <guilhermefragnani@icloud.com>
2022-06-24 11:13:32 -0300

    adicionado endpoind o pointcloud

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+
+struct Lista {
+    values: [i32;10],
+    ultimo: i32
+}
+
+fn criar_lista() -> Lista {
+    Lista {
+        values: [0;10],
+        ultimo: 0
+    }
+}
+
+fn main() {
+    
+    
+}
