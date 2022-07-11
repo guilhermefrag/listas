ece52ac4df3b4bbe486b2e18add4acc3dbd4c8b0
guilhermefrag <guilhermefragnani@icloud.com>
2022-07-11 15:34:17 -0300

    enviando

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-
 struct Lista {
     values: [i32;10],
-    ultimo: i32
+    ultimo: usize
 }
 
 fn criar_lista() -> Lista {
@@ -10,6 +9,33 @@ fn criar_lista() -> Lista {
         ultimo: 0
     }
 }
+
+fn lista_cheia(lista: &Lista) -> bool {
+    if lista.ultimo == 9 {
+        return true
+    } else {
+        return false
+    }
+}
+
+fn lista_vazia(lista: &Lista) -> bool {
+    if lista.ultimo == 0 {
+        return true
+    } else {
+        return false
+    }
+}
+
+fn insere_lista(lista: &mut Lista, valor: i32) {
+    if lista_cheia(lista) {
+        println!("Lista cheia");
+    } else {
+        lista.values[lista.ultimo] = valor;
+        lista.ultimo += 1;
+    }
+}
+
+
 
 fn main() {
     
